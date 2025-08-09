ce468632ecb7577aee77bece828e399fcd7a180d
Lev Roskoshin <lev.roskoshin@myexlab.com>
2025-08-09 17:09:31 +0200

    chore: fix profile dummy

--- libs/twitter/src/twitter.service.ts
+++ libs/twitter/src/twitter.service.ts
@@ -95,7 +95,7 @@ export class TwitterService {
   })
   private async getGuestToken(): Promise<string | undefined> {
     this.logger.log('Getting new guest token');
-    const response = await axios.get('https://x.com/GMX_IO', {
+    const response = await axios.get('https://x.com/elonmusk', {
       headers: {
         accept:
           'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
